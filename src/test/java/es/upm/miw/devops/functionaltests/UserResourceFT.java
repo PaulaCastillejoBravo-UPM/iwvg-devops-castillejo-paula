b20e0f6a602f6cef274c8eb53bad21286134d444
Paula <Paula.Castillejo1@alu.uclm.es>
2026-09-12 10:48:30 +0200

    Fixed mvn -B verify in continuous-integration.

--- src/test/java/es/upm/miw/devops/functionaltests/UserResourceFT.java
+++ src/test/java/es/upm/miw/devops/functionaltests/UserResourceFT.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import es.upm.miw.devops.es.upm.api.UserController;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
@@ -15,10 +16,13 @@ class UserResourceFT {
     @Autowired
     private WebTestClient webTestClient;
 
+    @Autowired
+    private UserController userController;
+
     @Test
     void testFindById() {
         webTestClient.get()
-                .uri("/user/11111111-1111-1111-1111-111111111111")
+                .uri("/users/11111111-1111-1111-1111-111111111111")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
